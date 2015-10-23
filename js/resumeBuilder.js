0b2009fdc05c359327b1a3812dd68307e3648eac
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2015-10-22 22:09:48 -0700

    Prepend name and desired role to page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var name = "Emmanuoel Haroutunian";
+var role = "Developer";
+//Replace generic helper.js data with variables containing personalized info
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
